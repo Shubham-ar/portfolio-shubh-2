2b974b79af046a64193d3e0ecbf18a8b5ed9c6bc
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2024-01-21 23:36:19 -0500

    init commit

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return <div>I am a header</div>;
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,25 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1> Shubh's Portfolio</h1>
 
-      <h2 className="text-red-500">Hello World in Red</h2>
+      <h2 className="text-blue-500">Hello World in Red</h2>
+
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
     </>
   );
 }

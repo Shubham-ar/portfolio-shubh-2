a8456489deb92589e54c33e1d204bec650950c43
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2024-01-22 00:29:40 -0500

    Header Template

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,18 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1> Shubh's Portfolio</h1>
-
-      <h2 className="text-blue-500">Hello World in Red</h2>
-
-      {/* Header */}
       <Header />
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
       {/* About */}
       {/* Experience */}
       {/* Skills */}

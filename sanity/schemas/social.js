5862e34c2c37ae48738e2627442d479f88c62510
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2024-01-28 03:34:43 -0500

    final- sanity schemas , necessary imports and adj.

---
+++ sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export const post = {
+  name: "social",
+  title: "Social",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of Platform for social media",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "url",
+      type: "url",
+    },
+  ],
+};

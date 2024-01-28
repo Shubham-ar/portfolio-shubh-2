3e37bc337f4b7aec649df65b6eac269680f39daa
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2024-01-28 01:42:14 -0500

    Complete templates w/o CMS

---
+++ components/ContactMe.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Props = {};
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+const ContactMe = (props: Props) => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:shubham.yyz@aol.com?subject=${formData.subject}&body==Hi, my mame is ${formData.name}. ${formData.message}(${formData.email})`;
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col text-center md:text-left xl:flex-row  max-w-7xl px-10 min-h-screen justify-evenly xl:space-y-0 mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        PROJECTS
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I have just what you need.
+          <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
+        </h4>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">+1(647)7030295</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">South Etobicoke, ON</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">sjhamb.03@gmail.com</p>
+          </div>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 mx-auto w-fit"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
+          </div>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ContactMe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap  overflow-scroll z-0 x">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap  overflow-y-scroll overflow-x-hidden z-0 
+    scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+    >
       <Header />
       {/* Hero */}
       <section id="hero" className="snap-start">

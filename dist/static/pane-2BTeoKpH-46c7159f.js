1b3b2458257bc1558f137f4acfe72cf5630ce3ab
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2024-01-29 00:17:39 -0500

    Re-established Sanity support w/o errors

---
+++ dist/static/pane-2BTeoKpH-46c7159f.js
@@ -0,0 +1,1 @@
+import{a as f}from"./sanity-5eb218d9.js";export{f as default};
---
+++ sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-29";
+
+export const dataset = "production";
+export const projectId = "bxogueyw";
+export const useCdn = false;
